fe8c28075b2032bb92492b1a0769b3dcd60d65bf
Gihyaa <felipevivenciorodrigues@gmail.com>
2025-11-07 19:48:45 -0300

    03/11/2025

---
+++ Programação de Aplicativos Mobile I/4° Bimestre/GridApp/REAME.md
@@ -0,0 +1,4 @@
+# Grid App
+---
+## App:
+![GridApp](https://github.com/user-attachments/assets/1edba6cb-ed55-4071-9dcf-1cba1336a483)
---
+++ Programação de Aplicativos Mobile I/4° Bimestre/GridApp/app/src/main/java/com/GridApp/gridapp/data/DataSource.kt
@@ -0,0 +1,33 @@
+package com.GridApp.gridapp.data
+
+import com.GridApp.gridapp.model.Topic
+import com.GridApp.gridapp.R
+
+object DataSource {
+    val topics = listOf(
+        Topic(R.string.architecture, 58, R.drawable.architecture),
+        Topic(R.string.crafts, 121, R.drawable.crafts),
+        Topic(R.string.business, 78, R.drawable.business),
+        Topic(R.string.culinary, 118, R.drawable.culinary),
+        Topic(R.string.design, 423, R.drawable.design),
+        Topic(R.string.fashion, 92, R.drawable.fashion),
+        Topic(R.string.film, 165, R.drawable.film),
+        Topic(R.string.gaming, 164, R.drawable.gaming),
+        Topic(R.string.drawing, 326, R.drawable.drawing),
+        Topic(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Topic(R.string.music, 212, R.drawable.music),
+        Topic(R.string.painting, 172, R.drawable.painting),
+        Topic(R.string.photography, 321, R.drawable.photography),
+        Topic(R.string.tech, 118, R.drawable.tech),
+        Topic(R.string.automotive, 200, R.drawable.automotive),
+        Topic(R.string.biology, 155, R.drawable.biology),
+        Topic(R.string.ecology, 188, R.drawable.ecology),
+        Topic(R.string.engineering, 210, R.drawable.engineering),
+        Topic(R.string.finance, 185, R.drawable.finance),
+        Topic(R.string.geology, 95, R.drawable.geology),
+        Topic(R.string.history, 230, R.drawable.history),
+        Topic(R.string.journalism, 130, R.drawable.journalism),
+        Topic(R.string.law, 175, R.drawable.law),
+        Topic(R.string.physics, 190, R.drawable.physics),
+    )
+}
